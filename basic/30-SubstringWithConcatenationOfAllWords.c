6a62cb5a1b2322b743ca3f82a856466b2d14b5a5
Hearen <LHearen@126.com>
2016-03-21 16:34:38 +0800

    30-SubstringWithConcatenationOfAllWord.c added with NOTHING

---
+++ basic/30-SubstringWithConcatenationOfAllWords.c
@@ -0,0 +1,18 @@
+/*******************************************
+Author      : LHearen
+E-mail      : LHearen@126.com
+Time        : 2016-02-25 10:44
+Description : You are given a string, s, and a list of words, words, that are all of the same length. Find all starting indices of substring(s) in s that is a concatenation of each word in words exactly once and without any intervening characters.
+
+For example, given:
+s: "barfoothefoobarman"
+words: ["foo", "bar"]
+
+You should return the indices: [0,9].
+(order does not matter).
+Source      : https://leetcode.com/problems/substring-with-concatenation-of-all-words/
+*******************************************/
+int* findSubstring(char* s, char** words, int size, int* returnSize)
+{
+
+}
