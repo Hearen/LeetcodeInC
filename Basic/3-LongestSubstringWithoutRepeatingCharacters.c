6f0b87cd5523129db01d2c3c19640e2fe8fcb978
Hearen <LHearen@126.com>
2016-03-21 10:19:08 +0800

    3-LongestSubstringWithoutRepeatingCharacters.c updated with comments

--- Basic/3-LongestSubstringWithoutRepeatingCharacters.c
+++ Basic/3-LongestSubstringWithoutRepeatingCharacters.c
@@ -1,73 +1,27 @@
 /*******************************************
-Author: LHearen
-E-mail: LHearen@126.com
-Time  :	2015-12-13 20:50
-Description: 
-Source: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+Author      : LHearen
+E-mail      : LHearen@126.com
+Time        : 2016-03-21 09:49
+Description : Given a string, find the length of the longest substring without repeating characters. For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
+Source      : https://leetcode.com/problems/longest-substring-without-repeating-characters/
 *******************************************/
-#include"utils.h"
-//rather ugly;
-#define T
-bool exists(char c, char* s, int size)
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define LEN 128
+int lengthOfLongestSubstring(char* s)
 {
-    for(int i = 0; i < size; i++)
+    if(s[0] == '\0') return 0;
+    int max = 1;
+    int map[LEN];
+    memset(map, -1, sizeof(int)*LEN);
+    for(int i=0, j=0; s[i]; i++)
     {
-        #ifdef T
-            printf("ei: %d\tec: %c\n", i, *(s+i));
-        #endif
-        if(c == *(s+i))
-            return true;
-    }
-    return false;
-}
-
-int lengthOfLongestSubstring(char *s)
-{
-    /*if(*s == '\0')*/
-    /*return 0;*/
-    /*if(*s != *(s+1))*/
-    /*return 1 + lengthOfLongestSubstring(s + 1);*/
-    /*else*/
-    /*return lengthOfLongestSubstring(s + 1);*/
-    int i = 0, count = 1;
-    while(*(s+i) != '\0')
-    {
-        count++;
-        i++;
-    }
-    #ifdef T0
-        printf("count: %d\n", count);
-    #endif
-    //count will include '\0';
-    char* tmp = (char*)malloc(sizeof(char)*count);
-    int max = 0;
-    int index = 0;
-    int i0 = 0;
-    count = 0;
-    i = 0;
-    while(*(s+i) != '\0')
-    {
-        #ifdef T
-            printf("i: %d\tc: %c\n", i, *(s+i));
-        #endif
-        if(!exists(*(s+i), tmp, index))
-        {
-            count++;
-            #ifdef T
-                printf("count: %d\n", count);
-            #endif
-            tmp[index++] = *(s+i);
-            if(count > max)
-                max = count;
-        }
-        else
-        {
-            count = 1;
-            index = 0;
-            i0++;
-            i = i0 - 1;
-        }
-        i++;
+        int index = s[i];
+        if(map[index] > -1) //duplicate;
+            j = MAX(map[index]+1, j); //to make duplicate case exactly the same as no-duplicate, add another 1 for adjustment;
+        map[index] = i;
+        int len = i-j+1; //no duplicate;
+        if(len > max)
+            max = len;
     }
     return max;
 }
