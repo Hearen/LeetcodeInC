770d52ec99db1fe65fb200387baf55c0a0ad16ea
Hearen <LHearen@126.com>
2016-03-21 10:47:22 +0800

    2-AddTwoNumbers.c

--- easy/2-AddTwoNumbers.c
+++ easy/2-AddTwoNumbers.c
@@ -2,7 +2,9 @@
 Author      : LHearen
 E-mail      : LHearen@126.com
 Time        : 2016-02-28 09:48
-Description : 
+Description : You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
 Source      : https://leetcode.com/problems/add-two-numbers/
 *******************************************/
 struct ListNode
