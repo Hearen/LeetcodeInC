d1548b1791840377a46a1068f53a00cc102864ad
Hearen <LHearen@126.com>
2016-03-21 14:55:06 +0800

    23-MergeKSortedLists.c

---
+++ basic/23-MergeKSortedLists.c
@@ -0,0 +1,64 @@
+/*******************************************
+Author      : LHearen
+E-mail      : LHearen@126.com
+Time        : 2015-12-24 21:20
+Description : Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+Source      : https://leetcode.com/problems/merge-k-sorted-lists/
+*******************************************/
+#include <stdlib.h>
+struct ListNode
+{
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode* merge(struct ListNode* l, struct ListNode* r)
+{
+    struct ListNode head;
+    struct ListNode *p = &head;
+    while(l && r)
+    {
+        if(l->val <= r->val)
+        {
+            p->next = l;
+            l = l->next;
+        }
+        else
+        {
+            p->next = r;
+            r = r->next;
+        }
+        p = p->next;
+    }
+    p->next = (l? l : r);
+    return head.next;
+}
+
+//AC - 388ms;
+//multi-thread programming might be helpful in this case;
+struct ListNode* mergeKLists0(struct ListNode** lists, int listsSize)
+{
+    if(listsSize == 0) return NULL;
+    struct ListNode** stack0 = (struct ListNode**)malloc(sizeof(struct ListNode*) * listsSize);
+    struct ListNode** stack1 = (struct ListNode**)malloc(sizeof(struct ListNode*) * listsSize);
+    int top0 = -1, top1 = -1;
+    for(int i = 0; i < listsSize; i++)
+        stack0[++top0] = lists[i];
+    while(top0 > 0)
+    {
+        while(top0 > -1)
+        {
+            if(top0 > 0)
+                stack1[++top1] = merge(stack0[top0--], stack0[top0--]);
+            else
+                stack1[++top1] = stack0[top0--];
+        }
+        top0 = top1;
+        top1 = -1;
+        struct ListNode** tmp = stack0;
+        stack0 = stack1;
+        stack1 = tmp;
+    }
+    return stack0[top0];
+}
+
