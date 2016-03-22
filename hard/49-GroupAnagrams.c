775ad74b31b7519d5d4a335c59f85d53633b7bea
Hearen <LHearen@126.com>
2016-03-22 15:12:16 +0800

    should be handled in time

---
+++ hard/49-GroupAnagrams.c
@@ -0,0 +1,22 @@
+/*******************************************
+Author      : LHearen
+E-mail      : LHearen@126.com
+Time        : 2016-03-06 21:32
+Description : Given an array of strings, group anagrams together.
+For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"], 
+Return:
+[
+["ate", "eat","tea"],
+["nat","tan"],
+["bat"]
+]
+Note:
+For the return value, each inner list's elements must follow the lexicographic order.
+All inputs will be in lower-case.
+Source      : https://leetcode.com/problems/anagrams/
+*******************************************/
+#include <stdlib.h>
+
+char*** groupAnagrams(char** strs, int size, int** colSizes, int* returnSize)
+{
+}
