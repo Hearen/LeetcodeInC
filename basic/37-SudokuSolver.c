d8b804313357c516cc3dfacb1c30db2dbc7fc9cd
Hearen <LHearen@126.com>
2016-03-22 10:02:05 +0800

    37-SudokuSolver.c updated with optimized

--- basic/37-SudokuSolver.c
+++ basic/37-SudokuSolver.c
@@ -17,7 +17,7 @@ bool check(char** board, int r, int c, char a) //check it in row, column and the
             if(board[r0+i][c0+j] == a) return false;
     return true;
 }
-bool solver(char** board, int r, int c) //check and try it from left to right and then downwards;
+bool solver0(char** board, int r, int c) //check and try it from left to right and then downwards;
 {
     if(r == 9) return true; //now till the end and nothing wrong so far, so return true;
     if(c == 9) return solver(board, r+1, 0); //till the end of the current row and then downwards;
@@ -33,7 +33,46 @@ bool solver(char** board, int r, int c) //check and try it from left to right an
     }
     return false; //nothing works around then return false;
 }
-void solveSudoku(char** board, int rSize, int cSize)
+//AC - 12ms;
+void solveSudoku0(char** board, int rSize, int cSize)
 {
     solver(board, 0, 0); //start from the top-left position to the bottom right end;
+}
+
+bool solver(char** board, int r, int c, bool rTaken[][9], bool cTaken[][9], bool subTaken[][9]) //check and try it from left to right and then downwards;
+{
+    if(r == 9) return true; //now till the end and nothing wrong so far, so return true;
+    if(c == 9) return solver(board, r+1, 0, rTaken, cTaken, subTaken); //till the end of the current row and then downwards;
+    if(board[r][c] != '.') return solver(board, r, c+1, rTaken, cTaken, subTaken); //already taken just move to the next;
+    for(char a = '1'; a <= '9'; a++) //try each possible candidate;
+    {
+        int num=a-'1', k=r/3*3+c/3;
+        if(!(rTaken[r][num] || cTaken[c][num] || subTaken[k][num])) //check its validity;
+        {
+            rTaken[r][num] = cTaken[c][num] = subTaken[k][num] = true;
+            board[r][c] = a; //set it then and just move to the next;
+            if(solver(board, r, c+1, rTaken, cTaken, subTaken)) return true; //if it's solvable then just return true;
+            board[r][c] = '.'; //restore it for easier checking in the next round;
+            rTaken[r][num] = cTaken[c][num] = subTaken[k][num] = false;
+        }
+    }
+    return false; //nothing works around then return false;
+}
+
+//AC - 4ms;
+void solveSudoku(char** board, int rSize, int cSize)
+{
+    bool rTaken[9][9]={0}, cTaken[9][9]={0}, subTaken[9][9]={0};
+    for(int r = 0; r < rSize; r++)
+    {
+        for(int c = 0; c < cSize; c++)
+            if(board[r][c] != '.')
+            {
+                int num=board[r][c]-'1', k=r/3*3+c/3;
+                rTaken[r][num] = true;
+                cTaken[c][num] = true;
+                subTaken[k][num] = true;
+            }
+    }
+    solver(board, 0, 0, rTaken, cTaken, subTaken); //start from the top-left position to the bottom right end;
 }
