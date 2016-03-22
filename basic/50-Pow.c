40c4f91f627cfabaad264c5dba9ec3ebc35056aa
Hearen <LHearen@126.com>
2016-03-22 15:38:05 +0800

    50-Pow.c two solutions

---
+++ basic/50-Pow.c
@@ -0,0 +1,45 @@
+/*******************************************
+Author      : LHearen
+E-mail      : LHearen@126.com
+Time        : 2015-12-24 08:45
+Description : 
+Source      : https://leetcode.com/problems/powx-n/
+*******************************************/
+//AC - 0ms;
+//divide and conquer;
+double myPow0(double x, int n)
+{
+    if(n == 0)
+        return 1;
+    long n0 = n;
+    if(n < 0)
+    {
+        x = 1/x;
+        n0 *= -1;
+    }
+    return n0%2 == 0? myPow(x*x, n0/2) : myPow(x*x, n0/2) * x;
+}
+
+//AC - 0ms;
+//bit-manipulation;
+double myPow1(double x, int n)
+{
+    if(n == 0)
+        return 1;
+    long n0 = n;
+    if(n < 0)
+    {
+        x = 1/x;
+        n0 *= -1;
+    }
+    long b = n0;
+    double res = 1;
+    while(b > 0)
+    {
+        if(b & 1)
+            res *= x;
+        x *= x;
+        b>>=1;
+    }
+    return res;
+}
