d82bf5257409e1a3454111ded85d30c14a21b01b
Hearen <LHearen@126.com>
2016-03-22 17:25:39 +0800

    54-SpiralMatrix.c with two solutions

--- basic/54-SpiralMatrix.c
+++ basic/54-SpiralMatrix.c
@@ -2,7 +2,16 @@
 Author      : LHearen
 E-mail      : LHearen@126.com
 Time        : 2016-02-26 08:42
-Description : 
+Description : Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+For example,
+Given the following matrix:
+
+[
+[ 1, 2, 3  ],
+[ 4, 5, 6  ],
+[ 7, 8, 9  ]
+]
+You should return [1,2,3,6,9,8,7,4,5].
 Source      : https://leetcode.com/problems/spiral-matrix/
 *******************************************/
 #include <stdlib.h>
