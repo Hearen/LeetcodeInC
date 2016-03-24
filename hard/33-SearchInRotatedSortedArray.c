95d38716a15f0f4633cb284ed8754f31cd7c37cb
Hearen <LHearen@126.com>
2016-03-24 11:34:50 +0800

    33-SearchInRotatedSortedArray.c added with seveval different solutions:

--- hard/33-SearchInRotatedSortedArray.c
+++ hard/33-SearchInRotatedSortedArray.c
@@ -8,7 +8,64 @@ You are given a target value to search. If found in the array return its index, 
 You may assume no duplicate exists in the array.
 Source      : https://leetcode.com/problems/search-in-rotated-sorted-array/
 *******************************************/
+int searchIndex0(int* nums, int begin, int end, int target)
+{
+    if(end < begin) return -1;
+    int m = (begin+end)/2;
+    int mid = nums[m];
+    if(nums[m] == target) return m;
+    else
+    {
+        int right = searchIndex(nums, m+1, end, target);
+        return right==-1? searchIndex(nums, begin, m-1, target) : right;
+    }
+}
+
+//AC - 0ms;
+int search0(int* nums, int size, int target)
+{
+    return searchIndex(nums, 0, size-1, target);
+}
+
+int search1(int* nums, int size, int target)
+{
+    int l=0, r=size-1;
+    while(l < r)
+    {
+        int m = (l+r)/2;
+        if(nums[m] == target) return m;
+        if(nums[l] <= nums[m])
+        {
+            if(nums[l]<=target && target<nums[m]) r = m-1;
+            else l = m+1;
+        }
+        else
+        {
+            if(nums[m]<target && target<=nums[r]) l = m+1;
+            else r = m-1;
+        }
+    }
+    return nums[l]==target? l : -1;
+}
+
 int search(int* nums, int size, int target)
 {
-
+    int l=0, r=size-1;
+    while(l < r)
+    {
+        int m=(l+r)/2;
+        if(nums[m]>nums[r]) l=m+1;
+        else r=m;
+    }
+    int s = l;
+    l=0, r=size-1;
+    while(l <= r)
+    {
+        int m = (l+r)/2;
+        int real = (m+s)%size;
+        if(nums[real] == target) return real;
+        if(nums[real] < target) l=m+1;
+        else r=m-1;
+    }
+    return -1;
 }
