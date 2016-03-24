9ef2992a6b013044ee07115e9a66a0a00780d96c
Hearen <LHearen@126.com>
2016-03-24 15:16:20 +0800

    81-SearchInRotatedSortedArray2.c basic but funny

--- hard/81-SearchInRotatedSortedArray2.c
+++ hard/81-SearchInRotatedSortedArray2.c
@@ -9,7 +9,26 @@ Would this affect the run-time complexity? How and why?
 Write a function to determine if a given target is in the array.
 Source      : https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
 *******************************************/
+#include <cstdbool.h>
 bool search(int* nums, int size, int target)
 {
-
+    int l=0, r=size-1;
+    while(l < r)
+    {
+        int m = (l+r)/2;
+        if(nums[m] == target) return true;
+        while(nums[l]==nums[m] && l!=m) l++;
+        while(nums[r]==nums[m] && r!=m) r--;
+        if(nums[l] <= nums[m])
+        {
+            if(nums[l]<=target && target<nums[m]) r = m-1;
+            else l = m+1;
+        }
+        else
+        {
+            if(nums[m]<target && target<=nums[r]) l = m+1;
+            else r = m-1;
+        }
+    }
+    return nums[l]==target;
 }
