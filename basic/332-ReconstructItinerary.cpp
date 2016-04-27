3c594c2d0502bfe68cb21f0591f39a955177a0d6
Hearen <LHearen@126.com>
2016-04-27 16:41:51 +0800

    332-ReconstructItinerary.cpp DFS using C++ multiset and undered_map

--- basic/332-ReconstructItinerary.cpp
+++ basic/332-ReconstructItinerary.cpp
@@ -22,26 +22,30 @@ Source      : https://leetcode.com/problems/reconstruct-itinerary/
 class Solution 
 {
 private: 
-    map<string, multiset<string>> targets;
+    unordered_map<string, multiset<string>> myGraph;
     vector<string> route;
-    void visit(string port)
+    void visit(string port) //DFS traversal to retrieve the route;
     {
-        while(targets[port].size())
+        //If k does not match the key of any element in the container, the function inserts a 
+        //new element with that key and returns a reference to its mapped value. Notice that 
+        //this always increases the container size by one, even if no mapped value is assigned 
+        //to the element (the element is constructed using its default constructor).
+        while(myGraph[port].size()) //the port exists in myGraph and there are others it can reach;
         {
-            string next = *targets[port].begin();
-            targets[port].erase(targets[port].begin());
-            visit(next);
+            string next = *myGraph[port].begin(); //retrieve the next from the beginning position to ensure lexical order;
+            myGraph[port].erase(myGraph[port].begin()); //once visited, erase it;
+            visit(next); //move to the next;
         }
-        route.push_back(port);
+        route.push_back(port); //add the destinations first and then the source;
     }
 public:
     //AC - 48ms;
     vector<string> findItinerary(vector<pair<string, string>> tickets)
     {
-        for(auto& ticket: tickets)
-            targets[ticket.first].insert(ticket.second);
-        visit("JFK");
-        return vector<string>(route.rbegin(), route.rend());
+        for(auto& ticket: tickets) //constructing the graph;
+            myGraph[ticket.first].insert(ticket.second);
+        visit("JFK"); //start from JFK;
+        return vector<string>(route.rbegin(), route.rend()); //reverse the route to the get the sequence;
     }
 
     //AC - 40ms;
@@ -50,7 +54,7 @@ public:
         unordered_map<string, multiset<string>> myGraph;
         int i, n = tickets.size();
         string first, second;
-        for(auto &ticket: tickets)
+        for(auto &ticket: tickets) //constructing the graph;
             myGraph[ticket.first].insert(ticket.second);
         vector<string> marching;
         vector<string> itinerary;
@@ -58,16 +62,17 @@ public:
         while(marching.size()>0) //all tickets are to be used though there might be different lexical orders;
         {
             string from = marching.back();
-            if(myGraph.count(from)>0 && myGraph[from].size()>0)
+            if(myGraph.count(from)) //not the final destination, push it to the stack;
             {
                 multiset<string>& to = myGraph[from]; //multiset will sort the strings automatically;
                 marching.push_back(*to.begin());
                 to.erase(to.begin());
+                if(to.empty()) myGraph.erase(from); //once the place has not any destinations more, delete it from the graph;
             }
             else
             {
-                itinerary.push_back(from);
-                marching.pop_back();
+                itinerary.push_back(from); //the destination - so far;
+                marching.pop_back(); //delete from the stack;
             }
         }
         reverse(itinerary.begin(), itinerary.end());
