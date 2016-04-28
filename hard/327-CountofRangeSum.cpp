a3cd56a9d9d49afeb1ffc86802cdd928a3520a6a
Hearen <LHearen@126.com>
2016-04-28 18:50:10 +0800

    327-CountofRangeSum.cpp five solutions

--- hard/327-CountofRangeSum.cpp
+++ hard/327-CountofRangeSum.cpp
@@ -2,7 +2,14 @@
 Author      : LHearen
 E-mail      : LHearen@126.com
 Time        : 2016-01-14 20:42
-Description : 
+Description : Given an integer array nums, return the number of range sums that lie in [lower, upper] inclusive.
+Range sum S(i, j) is defined as the sum of the elements in nums between indices i and j (i ≤ j), inclusive.
+Note:
+A naive algorithm of O(n2) is trivial. You MUST do better than that.
+Example:
+Given nums = [-2, 5, -1], lower = -2, upper = 2,
+Return 3.
+The three ranges are : [0, 0], [2, 2], [0, 2] and their respective sums are: -2, -1, 2.
 Source      : https://leetcode.com/problems/count-of-range-sum/
 *******************************************/
 #include <limits.h>
