813f1e55f73ab2de3705535957a01cdc7e53ccc1
Hearen <LHearen@126.com>
2016-06-07 21:27:38 +0800

    binary search

---
+++ cpp/34-Search4Range.c
@@ -0,0 +1,44 @@
+/*******************************************
+Author      : LHearen
+E-mail      : LHearen@126.com
+Time        : 2015-12-24 08:05
+Description : 
+Source      : https://leetcode.com/problems/search-for-a-range/
+*******************************************/
+//AC - 4ms;
+int* searchRange(int* nums, int numsSize, int target, int* returnSize)
+{
+    int l = -1, r = -1;
+    int *indexes = (int*)malloc(sizeof(int) * 2);
+    *returnSize = 2;
+    int low = 0, high = numsSize - 1;
+    int mid = 0;
+    while(low <= high)
+    {
+        mid = (low + high) / 2;
+        if(nums[mid] >= target)
+        {
+            high = mid - 1;
+            if(nums[mid] == target)
+                l = mid;
+        }
+        else 
+            low = mid + 1;
+    }
+    low = 0, high = numsSize - 1;
+    while(low <= high)
+    {
+        mid = (low + high) / 2;
+        if(nums[mid] <= target)
+        {
+            low = mid + 1;
+            if(nums[mid] == target)
+                r = mid;
+        }
+        else 
+            high = mid - 1;
+    }
+    indexes[0] = l;
+    indexes[1] = r;
+    return indexes;
+}
